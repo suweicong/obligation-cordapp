9f0b9fe50dd35ae3ac33928abee4b4b51cdfc5e3
Adrian Soon <adrian.soon@accenture.com>
2018-11-22 10:08:34 +0700

    Add replicated test

--- flow-v2/src/main/kotlin/flows/v2/QueryFlow.kt
+++ flow-v2/src/main/kotlin/flows/v2/QueryFlow.kt
@@ -1,2 +1,47 @@
 package flows.v2
 
+import co.paralleluniverse.fibers.Suspendable
+import com.base.ObligationDefinition
+import contract.states.v2.ObligationSchemaV2
+import net.corda.core.contracts.StateRef
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.PageSpecification
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.node.services.vault.Sort
+import net.corda.core.node.services.vault.SortAttribute
+
+@InitiatingFlow
+@StartableByRPC
+class QueryFlow(val stateRefs: List<StateRef>,
+                val pageNumber: Int,
+                val pageSize: Int) : FlowLogic<Vault.Page<ObligationDefinition>>() {
+    @Suspendable
+    override fun call(): Vault.Page<ObligationDefinition> {
+
+        logger.info("stateRefs: $stateRefs")
+        logger.info("pageNumber: $pageNumber")
+        logger.info("pageSize: $pageSize")
+
+
+        val sortAttribute = SortAttribute.Custom(ObligationSchemaV2.ObligationEntity::class.java, "amount")
+        val sort = Sort.Direction.ASC
+        val sorting = Sort(listOf(Sort.SortColumn(sortAttribute, sort)))
+        val vaultCriteriaForReturn = QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.ALL, stateRefs = stateRefs)
+        val pageSpec = PageSpecification(pageNumber = pageNumber, pageSize = pageSize)
+
+        // TODO: Putting sort in here will cause the duplicate metadata on 3.3 but works fine one 3.2
+        val result = serviceHub.vaultService.queryBy<ObligationDefinition>(vaultCriteriaForReturn, pageSpec, sorting)
+
+        logger.info("queryFlow result ${result.states.size}")
+        logger.info("queryFlow result totalStatesAvailable ${result.totalStatesAvailable}")
+        logger.info("queryFlow result $result")
+
+        return result
+    }
+
+
+}
--- src/test/kotlin/ObligationTestTwo.kt
+++ src/test/kotlin/ObligationTestTwo.kt
@@ -1,5 +1,8 @@
 import com.base.ObligationDefinition
+import flows.v2.QueryFlow
+import net.corda.core.contracts.StateAndRef
 import net.corda.core.node.services.queryBy
+import net.corda.core.utilities.getOrThrow
 import net.corda.finance.POUNDS
 import org.junit.Test
 
@@ -7,15 +10,43 @@ class TestTwoMixtureOfStates : BaseTest() {
 
     @Test
     fun `Create Obligation`() {
-        issueObligationV1(a, b, 1000.POUNDS, anonymous = false, remark = null)
         issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        issueObligationV2(a, b, 1000.POUNDS, anonymous = false, remark = null)
+        network.waitQuiescent()
 
+
+        var firstRound: List<StateAndRef<ObligationDefinition>> = emptyList()
+
+        // 1. Get the state and ref from the vault.
         a.transaction {
             val results = a.services.vaultService.queryBy<ObligationDefinition>()
-            println(results)
+            firstRound = results.states
 
-
+            println("firstRound ${results.states.size}")
+            println("firstRound ${results.totalStatesAvailable}")
+            println("firstRound $results")
         }
 
+        // 2. Query the state ref with page number/size and sorting.
+        val flow = QueryFlow(firstRound.map { it.ref }, 1, 10)
+        val flowResult = a.startFlow(flow).getOrThrow()
+
+        // I should get 10 results here, but you can see there are duplicate metadata in the vault.page
+        println("flowResult ${flowResult.states.size}")
+        println("flowResult ${flowResult.totalStatesAvailable}")
+        println("flowResult $flowResult")
     }
 }
